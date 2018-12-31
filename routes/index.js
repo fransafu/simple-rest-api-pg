bc4570fe2f61a281ca3b7a473d08890b5d8c106e
Francisco Javier Sánchez Fuentes <fransafu@gmail.com>
2018-12-31 17:25:41 -0300

    ADD: Se agrega lista de Endpoint para documentos

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,33 @@ const appRouter = function (app) {
   app.get("/", function(req, res) {
     res.status(200).send("Hola :)");
   })
+
+  // Agregando Endpoints para documentos
+  // GET /documents Obtiene todos los documentos
+  app.get("/documents", function(req, res) {
+    res.status(200).send("Obtener lista de documentos");
+  })
+
+  app.get("/documents/:id", function(req, res) {
+    res.status(200).send("Obtener documento especifico");
+  })
+
+  app.post("/documents", function(req, res) {
+    res.status(200).send("Documento creado con exito");
+  })
+
+  app.patch("/documents/:id", function(req, res) {
+    res.status(200).send("Actualización parcial de documento");
+  })
+
+  app.put("/documents/:id", function(req, res) {
+    res.status(200).send("Actualizar completamente el documento");
+  })
+
+  app.delete("/documents/:id", function(req, res) {
+    res.status(200).send("Documento eliminado");
+  })
+
 }
 
 // Exporta la funcion para su uso
